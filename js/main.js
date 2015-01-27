2788e573337c0fe7578c0168540bd460d4d97170
Vinícius Ribeiro <vl.ribeiro3@gmail.com>
2015-01-27 01:29:10 -0200

    Fix wide image exhibition

--- js/main.js
+++ js/main.js
@@ -1,81 +1,100 @@
-( function( window ) {
+( function( window, $ ) {
 
-'use strict';
+  'use strict';
 
-//private content
+  //private content
 
-var menuHidden = false;
+  var menuHidden = false;
 
-function headerConfig() {
-  var header = $('header');
-  var content = $('.content');
-  var searchButton = $('header button');
-  var closeSearchButton = $('.close-button');
+  function headerConfig() {
+    var header = $('header');
+    var content = $('.content');
+    var searchButton = $('header button');
+    var closeSearchButton = $('.close-button');
 
-  //adjust content padding
-  content.css('padding-top', header.height());
+    //adjust content padding
+    content.css('padding-top', header.height());
 
-  $(document).on('scroll', documentScroll);
-  searchButton.on('click', clickSearch);
-  closeSearchButton.on('click', clickCloseSearch);
-}
-
-function documentScroll(e) {
-  if ($('body').scrollTop() <= 100) {
-    menuHidden = false;
-
-    $('header nav').removeClass('nav-hidden');
+    $(document).on('scroll', documentScroll);
+    searchButton.on('click', clickSearch);
+    closeSearchButton.on('click', clickCloseSearch);
   }
-  else if (!menuHidden) {
-    menuHidden = true;
 
-    $('header nav').addClass('nav-hidden');
-  }
-}
+  function documentScroll(e) {
+    if ($('body').scrollTop() <= 100) {
+      menuHidden = false;
 
-function clickSearch(e) {
-  $('.search-form').addClass('active');
-}
+      $('header nav').removeClass('nav-hidden');
+    }
+    else if (!menuHidden) {
+      menuHidden = true;
 
-function clickCloseSearch(e) {
-  $('.search-form').removeClass('active');
-}
-
-function responsiveYoutubeConfig() {
-  var contentParagraphs = document.querySelectorAll('.post-content p');
-
-  for (var i = 0; i < contentParagraphs.length; i++) {
-    var currParagraph = contentParagraphs[i];
-
-    if (currParagraph.querySelector('iframe')) {
-      classie.add(currParagraph, 'video-wrapper');
+      $('header nav').addClass('nav-hidden');
     }
   }
-}
 
-//public content
+  function clickSearch(e) {
+    $('.search-form').addClass('active');
+  }
 
-var init;
+  function clickCloseSearch(e) {
+    $('.search-form').removeClass('active');
+  }
 
-init = function() {
-  headerConfig();
-  responsiveYoutubeConfig();
-}
+  function responsiveYoutubeConfig() {
+    var contentParagraphs = document.querySelectorAll('.post-content p');
 
-var ba = {
-  init: init,
-};
+    for (var i = 0; i < contentParagraphs.length; i++) {
+      var currParagraph = contentParagraphs[i];
 
-// transport
-if ( typeof define === 'function' && define.amd ) {
-  // AMD
-  define( ba );
-} else if ( typeof exports === 'object' ) {
-  // CommonJS
-  module.exports = ba;
-} else {
-  // browser global
-  window.ba = ba;
-}
+      if (currParagraph.querySelector('iframe')) {
+        classie.add(currParagraph, 'video-wrapper');
+      }
+    }
+  }
 
-})( window );
+  function wideImageAdjust() {
+    var featuredImages = $('.header-featured-image');
+
+    for (var i in featuredImages) {
+      var featuredImage = $(featuredImages[i]);
+
+      if (featuredImage.height() < featuredImage.parent().height()) {
+        featuredImage.addClass('wide-image');
+
+        var negativeMargin = (featuredImage.width() - featuredImage.parent().width()) / 2;
+
+        featuredImage.css('left', '-' + negativeMargin + 'px');
+      }
+    }
+  }
+
+  //public content
+
+  var init;
+
+  init = function() {console.log('1');
+    $(function($) {console.log('2');
+      headerConfig();
+      responsiveYoutubeConfig();
+      wideImageAdjust();
+    });
+  }
+
+  var ba = {
+    init: init,
+  };
+
+  // transport
+  if ( typeof define === 'function' && define.amd ) {
+    // AMD
+    define( ba );
+  } else if ( typeof exports === 'object' ) {
+    // CommonJS
+    module.exports = ba;
+  } else {
+    // browser global
+    window.ba = ba;
+  }
+
+})( window, Zepto );
